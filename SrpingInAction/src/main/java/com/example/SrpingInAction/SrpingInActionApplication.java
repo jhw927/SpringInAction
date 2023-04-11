3bb76ebce338ac7c69e1b9461f7e293acad2c46c
JoHyunwoo <jhw3874@gmail.com>
2023-04-11 23:29:38 +0900

    Ch01

---
+++ SrpingInAction/src/main/java/com/example/SrpingInAction/SrpingInActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.SrpingInAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingInActionApplication.class, args);
+	}
+
+}
